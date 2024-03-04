d17acd3245738700506949bd2f9ca65d426bbe0d
bulefillee <114043428+bulefillee@users.noreply.github.com>
2024-03-04 01:59:02 -0500

    styles & content
    added styles & content

--- 404.php
+++ 404.php
@@ -1,3 +1,7 @@
-<?php get_header(); ?>
-<p> This is a 404 page</p> 
+<?php get_header('simple'); ?>
+<div class="error-box">
+<h1>404</h1>
+<p>Oops! Something is wrong.</p>
+<a class="button" href=" https://www.creativesbdesign.com/idm250/"><i class="icon-home"></i> Go back Home</a>
+</div>
 <?php get_footer(); ?>
---
+++ page-custom1.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Template Name: Custom Template 1
+*/
+?>
+
+<?php get_header(); ?>
+<h1>
+  <?php echo get_the_title(); ?>
+</h1>
+<p>This is a custom 2 page.php template</p>
+<?php get_footer(); ?>
