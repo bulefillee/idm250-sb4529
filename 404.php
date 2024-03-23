43d4603f3470a478733928dbc8ffa70bdfecdd38
bulefillee <114043428+bulefillee@users.noreply.github.com>
2024-03-22 22:23:57 -0400

    Final 404 fixes
    File updated with correct way to get url

--- 404.php
+++ 404.php
@@ -1,7 +1,9 @@
 <?php get_header('simple'); ?>
 <div class="error-box">
-<h1>404</h1>
-<p>Oops! Something is wrong.</p>
-<a class="button" href=" https://www.creativesbdesign.com/idm250/"><i class="icon-home"></i> Go back Home</a>
+    <h1 class="error-h1">404</h1>
+    <p>Oops! Something is wrong.</p>
+    <button class="error-button">
+        <a href="<?php echo esc_url(get_site_url()); ?>">Go back Home</a>
+    </button>
 </div>
 <?php get_footer(); ?>
